473d17c8060a8605505c2f96fb83778f9ee1c9f6
arunthampi <arun.thampi@gmail.com>
2008-08-18 11:38:41 +0800

    Added example for eventmachine. Get now accepts 'Method' objects apapart from 'Proc' objects

---
+++ eventmachine_example.rb
@@ -0,0 +1,24 @@
+# Most of this code is stolen from Igvita's excellent blog post on EventMachine:
+# http://www.igvita.com/2008/05/27/ruby-eventmachine-the-speed-demon/
+
+require 'evented_net'
+require 'evma_httpserver'
+
+class Handler  < EventMachine::Connection
+  include EventMachine::HttpServer
+
+  def process_evented_http_req(code, body)
+    puts "Code: #{code} Body: #{body}"
+  end
+ 
+  def process_http_request
+    uri = URI.parse('http://www.google.com')
+    EventedNet::HTTP.get(uri, :callback => method(:process_evented_http_req))
+  end
+end
+
+EventMachine::run {
+  EventMachine.kqueue
+  EventMachine::start_server("0.0.0.0", 8082, Handler)
+  puts "Listening"
+}
--- lib/http/get.rb
+++ lib/http/get.rb
@@ -2,16 +2,16 @@ module EventedNet
   module HTTP
     module Get
       def get(uri, opts = {})
-        unless uri.is_a?(URI) && opts[:callback].is_a?(Proc) && opts[:callback].arity == 2
-          raise ArgumentError, "uri must be a URI and opts[:callback] must be a proc which takes 2 args"
+        unless uri.is_a?(URI) && (opts[:callback].is_a?(Proc) || opts[:callback].is_a?(Method)) && opts[:callback].arity == 2
+          raise ArgumentError, "uri must be a URI and opts[:callback] must be a Proc (or Method) which takes 2 args"
         end
         EM.reactor_running? ? evented_get(uri, opts) : synchronous_get(uri, opts)
       end
       
       private
         def synchronous_get(uri, opts = {})
-          resp = Net::HTTP.get_response(uri)
-          opts[:callback].call(resp.code, resp.body)
+          r = Net::HTTP.get_response(uri)
+          opts[:callback].call(r.code, r.body)
         end
         
         def evented_get(uri, opts = {})
@@ -19,11 +19,8 @@ module EventedNet
             :host => uri.host, :port => uri.port,
             :request => uri.path, :query => uri.query
           )
-          # Make the call back of evented_get, call the user-generated
-          # callback
-          http.callback do |response|
-            opts[:callback].call(response.status, response.content)
-          end
+          # Make the call back of evented_get, call the user-generated callback
+          http.callback { |r| opts[:callback].call(r[:status], r[:content]) }
         end
     end
   end
